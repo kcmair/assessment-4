0445ab116b6741fa36d021f19dca1dcdb28217e4
Keith Mair <mairck@gmail.com>
2022-02-11 12:13:36 -0700

    almost there

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
+let compliments = [
+  "Gee, you're a smart cookie!",
+  "Cool shirt!",
+  "Your Javascript skills are stellar.",
+];
+
+app.get("/api/compliment", (req, res) => {  
 
   // choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
@@ -20,6 +22,32 @@ app.get("/api/compliment", (req, res) => {
 
   res.status(200).send(randomCompliment);
   
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = [
+    "A fresh start will put you on your way.",
+    "From listening comes wisdom and from speaking repentance.",
+    "It is better to be an optimist and proven a fool than to be a pessimist and be proven right.",
+    "Physical activity will dramatically improve your outlook today.",
+    "Pick battles big enough to matter, small enough to win."
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+
+});
+
+app.put("/api/addCompliment", (req, res) => {
+  const request = req.body
+  const response = res.body
+  console.log(request)
+  console.log(response)
+
+  res.status(200).send(`${response} has been added to the list of compliments`);
 });
 
 app.listen(4000, () => console.log("Server running on 4000"));
